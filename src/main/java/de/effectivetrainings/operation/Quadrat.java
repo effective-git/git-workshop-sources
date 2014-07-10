e08e5e1475aaa6b3aaf9b3d7f4b82a3c87fcce84
dilgerm <martin@effectivetrainings.de>
2014-07-10 21:44:39 +0200

    new Operation Quadrat

---
+++ src/main/java/de/effectivetrainings/operation/Quadrat.java
@@ -0,0 +1,21 @@
+package de.effectivetrainings.operation;
+
+/**
+ * @author Martin Dilger
+ * @since: 10.07.14
+ */
+public class Quadrat implements Operation
+{
+
+	@Override
+	public Double perform(Double operandA, Double operandB)
+	{
+		return operandA * operandA;
+	}
+
+	@Override
+	public String getOperationIdentifier()
+	{
+		return "qdr";
+	}
+}
