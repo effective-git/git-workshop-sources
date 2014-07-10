5e1fa599ae4f5750a6e35ab1c4ba16386149c417
dilgerm <martin@effectivetrainings.de>
2014-07-10 21:48:40 +0200

    new Operation Cos

--- src/main/java/de/effectivetrainings/operation/Cos.java
+++ src/main/java/de/effectivetrainings/operation/Cos.java
@@ -4,18 +4,18 @@ package de.effectivetrainings.operation;
  * @author Martin Dilger
  * @since: 10.07.14
  */
-public class Quadrat implements Operation
+public class Cos implements Operation
 {
 
 	@Override
 	public Double perform(Double operandA, Double operandB)
 	{
-		return operandA * operandB;
+		return Math.cos(operandA);
 	}
 
 	@Override
 	public String getOperationIdentifier()
 	{
-		return "qdr";
+		return "cos";
 	}
 }
